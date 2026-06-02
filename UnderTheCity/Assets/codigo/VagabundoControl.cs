edbd35e0f915a0b39b0cd0a4da391bde6b0bcdc8
felipesaini-web <felipe.saini@usal.edu.ar>
2026-06-02 12:09:13 -0300

    codigo que funciona
    codigo que funciona

--- UnderTheCity/Assets/codigo/VagabundoControl.cs
+++ UnderTheCity/Assets/codigo/VagabundoControl.cs
@@ -1,50 +1,54 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class VagabundoControl : MonoBehaviour
 {
     public float velocidad = 5f;
     public float fuerzaSalto = 7f;
 
-    private Rigidbody rb;
+    private Rigidbody2D rb;
     private bool enSuelo;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         float movimiento = 0f;
 
-        if (Input.GetKey(KeyCode.A))
+        // Movimiento A / D
+        if (Keyboard.current.aKey.isPressed)
         {
             movimiento = -1f;
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Keyboard.current.dKey.isPressed)
         {
             movimiento = 1f;
         }
 
-        rb.linearVelocity = new Vector3(movimiento * velocidad, rb.linearVelocity.y, 0);
+        rb.linearVelocity = new Vector2(movimiento * velocidad, rb.linearVelocity.y);
 
-        if (Input.GetKeyDown(KeyCode.W) && enSuelo)
+        // Salto con W
+        if (Keyboard.current.wKey.wasPressedThisFrame && enSuelo)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, fuerzaSalto, 0);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, fuerzaSalto);
         }
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Piso"))
+        if (collision.gameObject.name == "Piso")
         {
             enSuelo = true;
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Piso"))
+        if (collision.gameObject.name == "Piso")
         {
             enSuelo = false;
         }
