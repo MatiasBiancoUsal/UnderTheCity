bcc9257e37a7bf24f62be520ff0a545f4995cfde
felipesaini-web <felipe.saini@usal.edu.ar>
2026-06-02 12:35:32 -0300

    rata
    rata

---
+++ UnderTheCity/Assets/codigo/RataControl.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class RataControl : MonoBehaviour
+{
+    public float velocidad = 5f;
+    public float fuerzaSalto = 7f;
+
+    private Rigidbody2D rb;
+    private bool enSuelo;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float movimiento = 0f;
+
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            movimiento = -1f;
+        }
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            movimiento = 1f;
+        }
+
+        rb.linearVelocity = new Vector2(movimiento * velocidad, rb.linearVelocity.y);
+
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame && enSuelo)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, fuerzaSalto);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        enSuelo = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        enSuelo = false;
+    }
+}
