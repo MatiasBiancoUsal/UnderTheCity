5cfde2eb68b1afdf233ad4b31363fa6f969bbb02
felipesaini-web <felipe.saini@usal.edu.ar>
2026-06-23 12:24:03 -0300

    se arreglo la palanca y el nivel 2

--- UnderTheCity/Assets/codigo/Palanca.cs
+++ UnderTheCity/Assets/codigo/Palanca.cs
@@ -5,13 +5,34 @@ public class Palanca : MonoBehaviour
 {
     public Trampilla trampilla;
 
+    public Sprite spriteApagado; 
+    public Sprite spriteEncendido; 
+
+    private SpriteRenderer sr;
     private bool jugadorCerca = false;
+    private bool activada = false;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
 
     private void Update()
     {
         if (jugadorCerca && Keyboard.current.eKey.wasPressedThisFrame)
         {
             trampilla.Activar();
+
+            activada = !activada;
+
+            if (activada)
+            {
+                sr.sprite = spriteEncendido;
+            }
+            else
+            {
+                sr.sprite = spriteApagado;
+            }
         }
     }
 
