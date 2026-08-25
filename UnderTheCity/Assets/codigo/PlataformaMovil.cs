948fb8e89bc508c1c7ceb2039183a618a4ddc4b5
tobiasmartinez17 <t.martinezgoya@usal.edu.ar>
2026-08-25 12:08:35 -0300

    los personajes al subir a la plataforma se mueven con ella

--- UnderTheCity/Assets/codigo/PlataformaMovil.cs
+++ UnderTheCity/Assets/codigo/PlataformaMovil.cs
@@ -15,11 +15,41 @@ public class PlataformaMovil : MonoBehaviour
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, objetivo.position, velocidad * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            objetivo.position,
+            velocidad * Time.deltaTime
+        );
 
         if (Vector2.Distance(transform.position, objetivo.position) < 0.1f)
         {
             objetivo = (objetivo == puntoA) ? puntoB : puntoA;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Vagabundo") ||
+            collision.gameObject.CompareTag("Rata"))
+        {
+            foreach (ContactPoint2D contacto in collision.contacts)
+            {
+                // El personaje está arriba de la plataforma
+                if (contacto.normal.y < -0.5f)
+                {
+                    collision.transform.SetParent(transform);
+                    return;
+                }
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Vagabundo") ||
+            collision.gameObject.CompareTag("Rata"))
+        {
+            collision.transform.SetParent(null);
         }
     }
 }
