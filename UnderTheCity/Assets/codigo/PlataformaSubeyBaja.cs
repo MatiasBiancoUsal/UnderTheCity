a870073b61d6d37fcd18abb41a1aa5c94c7bdbc0
felipesaini-web <felipe.saini@usal.edu.ar>
2026-09-09 22:39:32 -0300

    Nivel11

--- UnderTheCity/Assets/codigo/PlataformaSubeyBaja.cs
+++ UnderTheCity/Assets/codigo/PlataformaSubeyBaja.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlataformaSubeyBaja : MonoBehaviour
@@ -7,6 +8,14 @@ public class PlataformaSubeyBaja : MonoBehaviour
     public float velocidad = 2f;
 
     public bool activada = false;
+
+    private Vector3 posicionAnterior;
+    private List<Transform> jugadoresEncima = new List<Transform>();
+
+    private void Start()
+    {
+        posicionAnterior = transform.position;
+    }
 
     void Update()
     {
@@ -26,31 +35,52 @@ public class PlataformaSubeyBaja : MonoBehaviour
                 velocidad * Time.deltaTime
             );
         }
+
+        Vector3 desplazamiento = transform.position - posicionAnterior;
+
+        if (desplazamiento != Vector3.zero)
+        {
+            foreach (Transform jugador in jugadoresEncima)
+            {
+                if (jugador != null)
+                {
+                    jugador.position += desplazamiento;
+                }
+            }
+        }
+
+        posicionAnterior = transform.position;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Vagabundo") ||
-            collision.gameObject.CompareTag("Rata"))
+        if (!EsJugador(collision.gameObject))
+            return;
+
+        foreach (ContactPoint2D contacto in collision.contacts)
         {
-            foreach (ContactPoint2D contacto in collision.contacts)
+            if (contacto.normal.y < -0.5f)
             {
-                if (contacto.normal.y < -0.5f)
+                if (!jugadoresEncima.Contains(collision.transform))
                 {
-                    collision.transform.SetParent(transform);
-                    return;
+                    jugadoresEncima.Add(collision.transform);
                 }
+
+                return;
             }
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Vagabundo") ||
-            collision.gameObject.CompareTag("Rata"))
-        {
-            collision.transform.SetParent(null);
-        }
+        if (!EsJugador(collision.gameObject))
+            return;
+
+        jugadoresEncima.Remove(collision.transform);
     }
 
+    private bool EsJugador(GameObject objeto)
+    {
+        return objeto.CompareTag("Vagabundo") || objeto.CompareTag("Rata");
+    }
 }
--- UnderTheCity/Assets/codigo/Trampilla.cs
+++ UnderTheCity/Assets/codigo/Trampilla.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Trampilla : MonoBehaviour
@@ -7,6 +8,14 @@ public class Trampilla : MonoBehaviour
     public float velocidad = 2f;
 
     private bool activada = false;
+
+    private Vector3 posicionAnterior;
+    private List<Transform> jugadoresEncima = new List<Transform>();
+
+    private void Start()
+    {
+        posicionAnterior = transform.position;
+    }
 
     void Update()
     {
@@ -26,10 +35,57 @@ public class Trampilla : MonoBehaviour
                 velocidad * Time.deltaTime
             );
         }
+
+        Vector3 desplazamiento = transform.position - posicionAnterior;
+
+        if (desplazamiento != Vector3.zero)
+        {
+            foreach (Transform jugador in jugadoresEncima)
+            {
+                if (jugador != null)
+                {
+                    jugador.position += desplazamiento;
+                }
+            }
+        }
+
+        posicionAnterior = transform.position;
     }
 
     public void Activar()
     {
         activada = !activada;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!EsJugador(collision.gameObject))
+            return;
+
+        foreach (ContactPoint2D contacto in collision.contacts)
+        {
+            if (contacto.normal.y < -0.5f)
+            {
+                if (!jugadoresEncima.Contains(collision.transform))
+                {
+                    jugadoresEncima.Add(collision.transform);
+                }
+
+                return;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!EsJugador(collision.gameObject))
+            return;
+
+        jugadoresEncima.Remove(collision.transform);
+    }
+
+    private bool EsJugador(GameObject objeto)
+    {
+        return objeto.CompareTag("Vagabundo") || objeto.CompareTag("Rata");
     }
 }
